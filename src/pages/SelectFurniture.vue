d464ae9d082492572de739b7ab282f7cbd97a642
Romain <disenofranchute@gmail.com>
2023-02-28 18:27:51 -0300

    feat(select-furniture-page): css style for the page

--- src/pages/SelectFurniture.vue
+++ src/pages/SelectFurniture.vue
@@ -1,20 +1,15 @@
 <template>
   <h1>Selectiona que tipo de mueble quieres diseñar</h1>
-  <div class="furniture">
+  <router-link class="furniture" to="/create-furniture" @click="unlockNextPage">
     <h2 class="furniture--name">Librero unitario</h2>
     <img
       class="furniture--img"
       src="./../assets/img/librero-unitario.jpeg"
       alt=""
-    />
-  </div>
+  /></router-link>
   <div class="furniture">
     <h2 class="furniture--name">Pronto otras opciones...</h2>
-    <img class="furniture--img" src="" alt="" />
   </div>
-  <router-link to="/create-furniture" @click="unlockNextPage">
-    proxima etapa</router-link
-  >
 </template>
 
 <script>
@@ -31,4 +26,32 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  text-align: center;
+}
+.furniture {
+  width: 30vw;
+  height: 70vh;
+  border-radius: 3px;
+  border: solid;
+  position: relative;
+  margin: 1rem;
+  display: inline-block;
+  text-decoration: none;
+  color: black;
+}
+
+.furniture--name {
+  text-align: center;
+}
+
+.furniture--img {
+  position: absolute;
+  left: 50%;
+  max-width: 90%;
+  max-height: 80%;
+  border-radius: 5px;
+  transform: translateX(-50%);
+}
+</style>
