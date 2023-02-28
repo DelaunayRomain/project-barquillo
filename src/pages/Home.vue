c272469f9e09c2f00645ccbbbb9c1f3b20c6b7d0
Romain <disenofranchute@gmail.com>
2023-02-28 18:59:38 -0300

    feat(home-page): style and change home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>Muebles barquillo</h1>
-  <h3>Dibuja tu mueble soñado!</h3>
-  <router-link @click="stages.selectFurniture = true" to="/select-furniture"
-    >empezar</router-link
-  >
+  <section class="home">
+    <h1>Generador de cotizaciones para muebles barquillo</h1>
+    <h3>
+      Esta applicacion te permite dibujar y cotizar con nosotros un mueble a
+      medida. Que sea para soñar tu mueble perfecto, o porque realmente quieres
+      cotizarlo, ayudanos a mejorar esta app!
+    </h3>
+    <button>
+      <router-link @click="unlockNextPage" to="/reference" class="router-link"
+        >Empezar ahora &#x2794;
+      </router-link>
+    </button>
+  </section>
 </template>
 
 <script>
@@ -12,5 +20,40 @@ export default {
   computed: {
     ...mapGetters(['stages']),
   },
+  methods: {
+    unlockNextPage() {
+      this.stages.reference = true;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.home {
+  background-color: beige;
+  text-align: center;
+  height: 100vh;
+  padding-top: 10rem;
+}
+
+h1 {
+  font-size: 2rem;
+  padding-bottom: 3rem;
+}
+
+h3 {
+  margin-left: 20vw;
+  margin-right: 20vw;
+  padding-bottom: 2rem;
+}
+
+button {
+  transform: translateX(20rem);
+}
+
+.router-link {
+  text-decoration: none;
+  color: black;
+  font-size: 1.2rem;
+}
+</style>
